ca486bcc9805787368ba614e13f37cc22bd44ad7
mark5041 <mark.beloso5041@gmail.com>
2022-02-15 15:10:10 +0100

    first commit

---
+++ Movie.php
@@ -0,0 +1,30 @@
+<!-- 
+    ! esempio classe !
+
+    {
+        "adult": false,
+        "backdrop_path": "/bOtdLAnUxX3Hxn6LV5zFi15KW6S.jpg",
+        "genre_ids": [
+            28,
+            16,
+            18,
+            878,
+            12
+        ],
+        "id": 492999,
+        "original_language": "ja",
+        "original_title": "劇場版 進撃の巨人 覚醒の咆哮",
+        "overview": "Eren Yeager and others of the 104th Training Corps have just begun to become full members of the Survey Corps. As they ready themselves to face the Titans once again, their preparations are interrupted by the invasion of Wall Rose—but all is not as it seems as more mysteries are unraveled. As the Survey Corps races to save the wall, they uncover more about the invading Titans and the dark secrets of their own members.",
+        "popularity": 214.031,
+        "poster_path": "/5tcuowV4HZRxxvDiWn4bmumuZSk.jpg",
+        "release_date": "2018-01-13",
+        "title": "Attack on Titan: The Roar of Awakening",
+        "video": true,
+        "vote_average": 8.4,
+        "vote_count": 181
+    }, 
+
+-->
+
+<?php
+?>
